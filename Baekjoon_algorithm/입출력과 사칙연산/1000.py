035e0851e59cb3920bebe8283a8eeb5ea6e2114d
Juhwan <dev.juhwanlee@gmail.com>
2022-03-14 20:48:33 +0900

    백준 1000번

---
+++ Baekjoon_algorithm/입출력과 사칙연산/1000.py
@@ -0,0 +1,16 @@
+a, b = input().split()
+
+
+def sum():
+    numA = int(a)
+    numB = int(b)
+    return numA + numB
+
+
+print(sum())
+
+
+# 더 쉬운 풀이 방법
+a, b = input().split()
+
+print(int(a)+int(b))
