0f7fc3c76fc51751b6e7bdfd90cc021be348423e
Juhwan <dev.juhwanlee@gmail.com>
2022-03-14 21:23:03 +0900

    백준 10998, 1008, 10869번

---
+++ Baekjoon_algorithm/입출력과 사칙연산/10998, 1008, 10869.py
@@ -0,0 +1,20 @@
+# 10998번 https://www.acmicpc.net/problem/10998
+
+a, b = input().split()
+
+print(int(a) * int(b))
+
+# 1008번 https://www.acmicpc.net/problem/1008
+
+a, b = input().split()
+
+print(int(a) / int(b))
+
+# 10869번 https://www.acmicpc.net/problem/10869
+
+a, b = input().split()
+
+a = int(a)
+b = int(b)
+
+print(f'{a+b}\n{a-b}\n{a*b}\n{int(a/b)}\n{a%b}')
