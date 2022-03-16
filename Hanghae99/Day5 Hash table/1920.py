1f247e38234e17725f851ba4088c92f1460cdc05
Juhwan <dev.juhwanlee@gmail.com>
2022-03-16 15:41:02 +0900

    백준 1920번: use dict, read key

--- Hanghae99/Day5 Hash table/1920.py
+++ Hanghae99/Day5 Hash table/1920.py
@@ -5,7 +5,7 @@
 1 3 7 9 5      A안에 존재하는지 알아내자
 '''
 
-import sys
+'''import sys
 
 
 n = int(sys.stdin.readline())
@@ -24,8 +24,33 @@ for i in range(len(bList)):
     if bList[i] in intersection:
         print(1)
     else:
-        print(0)
+        print(0)'''
 
-## 시간초과
-#https://velog.io/@ledcost/%EB%B0%B1%EC%A4%80-10816-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EC%88%AB%EC%9E%90-%EC%B9%B4%EB%93%9C-2-%EC%8B%A4%EB%B2%844-%EC%9D%B4%EB%B6%84-%ED%83%90%EC%83%89
+# 시간초과
+# https://velog.io/@ledcost/%EB%B0%B1%EC%A4%80-10816-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EC%88%AB%EC%9E%90-%EC%B9%B4%EB%93%9C-2-%EC%8B%A4%EB%B2%844-%EC%9D%B4%EB%B6%84-%ED%83%90%EC%83%89
 ####
+
+
+import sys
+input = sys.stdin.readline
+#  변수로 받아서 사용가능하다
+
+n = int(input())
+# https://brownbears.tistory.com/37 파이썬 *(애스터리스크) 사용법
+aList = [*map(int, input().split())]
+m = int(input())
+bList = [*map(int, input().split())]
+
+count = {}
+for i in aList:
+    if i in count:
+        count[i] += 1
+    else:
+        count[i] = 1
+
+for target in bList:
+    result = count.get(target)
+    if result == None:
+        print(0, end=" ")
+    else:
+        print(result, end=" ")
