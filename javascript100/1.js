065abf234a45a3843384644c8aebde76ec05626f
Juhwan <dev.juhwanlee@gmail.com>
2022-03-26 16:41:25 +0900

    js 100제 1번

---
+++ javascript100/1.js
@@ -0,0 +1,17 @@
+/* 
+문제1 : 배열의 삭제
+다음 배열에서 400, 500를 삭제하는 code를 입력하세요.
+var nums = [100, 200, 300, 400, 500];
+*/
+
+const nums = [100, 200, 300, 400, 500];
+nums.splice(3, 4);
+
+console.log(nums);
+
+// 답안
+// var numsb = [100, 200, 300, 400, 500];
+// numsb.pop();
+// numsb.pop();
+
+// console.log(numsb);
