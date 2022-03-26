ab1740946048bb374d694f14679993c7f849a8d3
Juhwan <dev.juhwanlee@gmail.com>
2022-03-26 23:57:57 +0900

    js 100제 21번

---
+++ javascript100/21.js
@@ -0,0 +1,22 @@
+/*
+# 문제21 : set은 어떻게 만드나요?
+
+다음 중 set을 만드는 방법으로 올바른 것을 모두 고르시오.
+
+1)  var x = {1, 2, 3, 5, 6, 7};
+2)  var x = {};
+3)  var x = new Set('javascript');
+4)  var x = new Set(range(5));
+5)  var x = new Set();
+*/
+
+// 3,5
+
+/*
+# 답안
+
+3)  var x = new Set('javascript');
+5)  var x = new Set();
+
+정답은 '3번', '5번'입니다.
+*/
