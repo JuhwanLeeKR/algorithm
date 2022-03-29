c27d9cc40343287a58902741fc96d1ce5109a68b
Juhwan <dev.juhwanlee@gmail.com>
2022-03-29 16:04:31 +0900

    답안 예시 추가

--- javascript100/31-40/38.js
+++ javascript100/31-40/38.js
@@ -35,3 +35,26 @@ const solution = (input) => {
   for (let i in good) sum += gradeList[good[i]];
   console.log(sum);
 };
+
+/*
+const scores = prompt('점수입력').split(' ').map(function(n){
+  return parseInt(n, 10);
+});
+
+scores.sort((a, b) => {
+  return a-b;
+});
+
+let count = 0;
+let arr = [];
+
+while (arr.length < 3) {
+  let n = scores.pop();
+  if (!arr.includes(n)){
+    arr.push(n);
+  }
+  count += 1;
+}
+
+console.log(count);
+*/
