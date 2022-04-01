d58ade8f27f07f8caf0343daee2cdd788dda539a
Juhwan <dev.juhwanlee@gmail.com>
2022-04-01 17:00:00 +0900

    Add code: python bisect

--- Hanghae99/16-BinarySearch/704.py
+++ Hanghae99/16-BinarySearch/704.py
@@ -29,3 +29,21 @@ nums = [-1, 0, 3, 5, 9, 12]
 target = 9
 sol = Solution()
 print(sol.search(nums, target))
+
+
+"""
+책을 참고해보니
+파이썬에서는 이진 검색을 지원하는 bisect 모듈을 제공한다.
+
+https://docs.python.org/ko/3/library/bisect.html
+"""
+import bisect
+
+
+def search(self, nums: list[int], target: int):
+    index = bisect.bisect_left(nums, target)
+
+    if index < len(nums) and nums[index] == target:
+        return index
+    else:
+        return -1
